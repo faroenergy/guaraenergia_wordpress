9ea5b5a9904733a275097008b4d45574f431d862
Felipe Bebber <fbebber1@gmail.com>
2025-03-14 15:49:17 -0300

    desenv

--- wordpress/wp-content/themes/guaraenergia/head.php
+++ wordpress/wp-content/themes/guaraenergia/head.php
@@ -34,12 +34,12 @@
             font-family: "Graphik", sans-serif;
         }
     </style>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/core.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/core.css?v1">
     <?php if ($args['css'] == 'seja-um-parceiro'): ?>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
     <?php endif; ?>
     <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/favicon.svg">
-    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/<?php echo $args['css']; ?>.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/<?php echo $args['css']; ?>.css?v1" rel="stylesheet">
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-RB8551CLJ4"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
